3b5054472625f26566344807f60bef91a2114eaa
Berkay <berkaykw0@gmail.com>
2025-01-14 23:48:15 +0300

    deneme

---
+++ caffeine_meter/lib/caffeineRate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class deneme extends StatefulWidget {
+  const deneme({super.key});
+
+  @override
+  State<deneme> createState() => _denemeState();
+}
+
+class _denemeState extends State<deneme> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
